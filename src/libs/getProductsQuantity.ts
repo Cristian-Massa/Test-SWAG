c66d103cc0fdf6c30b7d9d080bc9f85b977bfb5f
Cristian-Massa <cristianmassa30@gmail.com>
2025-08-19 19:17:34 -0300

    refactor: cambio de getProductsuantity para tomar en cuenta el filtro 'all'

--- src/libs/getProductsQuantity.ts
+++ src/libs/getProductsQuantity.ts
@@ -3,7 +3,8 @@ import { Product } from "../types/Product";
 export function getProductsQuantity(products: Product[], cat: string) {
   let productQuantity = 0;
   products.forEach((product) => {
-    if (product.category === cat) productQuantity++;
+    if (product.category === cat || product.category === "all")
+      productQuantity++;
   });
 
   return productQuantity;
