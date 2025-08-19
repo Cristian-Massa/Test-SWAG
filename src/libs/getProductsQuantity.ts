f5608e68e021b6ac357fe162d2507c075ae6d241
Cristian-Massa <cristianmassa30@gmail.com>
2025-08-19 19:33:28 -0300

    feature: anadido ordenamiento por precio, nota: se cambio el switch por un objeto mapeado para mayor mantenibilidad, busqueda ya no es case sensitive

--- src/libs/getProductsQuantity.ts
+++ src/libs/getProductsQuantity.ts
@@ -1,11 +1,7 @@
 import { Product } from "../types/Product";
 
 export function getProductsQuantity(products: Product[], cat: string) {
-  let productQuantity = 0;
-  products.forEach((product) => {
-    if (product.category === cat || product.category === "all")
-      productQuantity++;
-  });
-
-  return productQuantity;
+  return cat === "all"
+    ? products.length
+    : products.filter((product) => product.category === cat).length;
 }
